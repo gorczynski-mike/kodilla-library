594d6282f4a6e2243daa2ef8cc41be3e5b4fbc9b
Mike <gorczynski.mike@gmail.com>
2018-08-14 09:02:34 +0200

    added datetime to/from sql.date converter

---
+++ src/main/java/com/kodilla/library/domain/converter/DateTimeConverter.java
@@ -0,0 +1,21 @@
+package com.kodilla.library.domain.converter;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Converter(autoApply = true)
+public class DateTimeConverter implements AttributeConverter<LocalDate, Date> {
+
+        @Override
+        public Date convertToDatabaseColumn(LocalDate locDate) {
+            return (locDate == null ? null : Date.valueOf(locDate));
+        }
+
+        @Override
+        public LocalDate convertToEntityAttribute(Date sqlDate) {
+            return (sqlDate == null ? null : sqlDate.toLocalDate());
+        }
+
+}
